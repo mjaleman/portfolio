956796de392285b7a2a7415cba83edbcf9b0c27e
Marlon Aleman <mjaleman1997@gmail.com>
2021-03-16 00:26:57 -0700

    Update projects to dynamically load more data on project click

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from projects import views
 
+app_name = 'projects'
+
 urlpatterns = [
-    path('', views.project_list),
+    path('', views.all_projects, name='all_projects'),
+    path('<int:pk>', views.project_detail, name='project_detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    # Query the DB to return all project objects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html', 
+            {'projects': projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
